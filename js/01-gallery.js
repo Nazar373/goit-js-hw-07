f54c42d7cf61fb380d9c2e76c71413fe80e72b00
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-07-30 23:13:27 +0300

    createGallery

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const images = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', images);
+
+gallery.addEventListener('click', modalWindow);
+
+function createGalleryItems(items) {
+  return items
+  .map(({preview, original, description}) => {
+    return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      </a>
+      </div>`
+  }).join("")
+};
+
+function modalWindow(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <img src="${e.target.dataset.source}">
+  `);
+  instance.show();
+  if (e.code === 'Escape' && instance.visible()) {
+    instance.close();
+  }
+  gallery.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+};
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,53 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+toCreateGallerySlide();
+
+var lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt' });
+
+function toCreateGallerySlide() {
+  const gallerySlide = galleryItems
+  .map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+  }).join('');
+  gallery.insertAdjacentHTML('beforeend', gallerySlide)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const gallery = document.querySelector('.gallery');
+
+// toCreateGallerySlide();
+
+// var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250 });
+
+// function toCreateGallerySlide() {
+//   const gallerySlide = galleryItems.map(({preview, original, description}) => {
+//    return `<a class="gallery__item" href="${original}">
+//     <img class="gallery__image" src="${preview}" alt="${description}">
+//     </a>`
+//    }).join('');
+//   gallery.insertAdjacentHTML('beforeend', gallerySlide)
+// }
